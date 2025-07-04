f7e84d40a82b7487724b1e29cdc7e2cd8321c9ca
lucas.sousa <lucas.sousa@codificar.com.br>
2025-07-04 13:53:33 -0300

    remove swegger app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiscordModule } from './discord/discord.module';
 import { PontoModule } from './ponto/ponto.module';
@@ -7,7 +6,6 @@ import { UtilModule } from './util/util.module';
 
 @Module({
   imports: [DiscordModule, PontoModule, UtilModule],
-  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
